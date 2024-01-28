40d0a493745610486722f8560ce3634711954f5d
Z0LTEKK <nabruno9@gmail.com>
2024-01-28 15:51:49 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("800x600")
+window.title("window!")
+icon = PhotoImage(file= "silk icons/src/bricks.png")
+window.iconphoto(True, icon)
+
+window.mainloop()
